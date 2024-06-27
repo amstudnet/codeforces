71f613e4b926fd115c13b51de18753b13d40e3be
amstudnet <86819045+amstudnet@users.noreply.github.com>
2024-06-27 16:50:09 +0800

    Create Maximum Increase.py

---
+++ Maximum Increase.py
@@ -0,0 +1,14 @@
+input()
+ans=list(map(int,input().split()))
+cnt=1
+max=1
+for i in range(1,len(ans)):
+    if ans[i]>ans[i-1]:
+        cnt+=1
+    else:
+        if cnt>max:
+            max=cnt
+        cnt=1
+if cnt>max:
+    max=cnt
+print(max)
